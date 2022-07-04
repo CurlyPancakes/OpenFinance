7dc48e2d33943849d2055f0364e5c6ac80b1c81b
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2022-07-04 22:33:36 +0200

    Clean up code

--- app/start.py
+++ app/start.py
@@ -1,12 +1,1 @@
-from libs.FingerPrint.fingerprint import FingerPrint
 
-fingerprint = FingerPrint()
-try:
-    fingerprint.open()
-    print("Please touch the fingerprint sensor")
-    if fingerprint.verify():
-        print("Authentication succeded!")
-    else:
-        print("Authentication failed!")
-finally:
-    fingerprint.close()
